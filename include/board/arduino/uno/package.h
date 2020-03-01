eebd343914da88605493e2d3d7536d973fa78818
hypothermic <admin@hypothermic.nl>
2020-03-01 19:10:21 +0100

    Add doxygen package group definitions
    Signed-off-by: hypothermic <admin@hypothermic.nl>

---
+++ include/board/arduino/uno/package.h
@@ -0,0 +1,8 @@
+/**
+ * @defgroup libepd2in9-uno Arduino Uno board compatibility module
+ *
+ * @brief Used to provide implementation for Arduino Uno R3
+ *
+ * @date 01-03-2020
+ * @author hypothermic <admin@hypothermic.nl>
+ */
---
+++ include/package.h
@@ -0,0 +1,8 @@
+/**
+ * @defgroup libepd2in9 LibEpd
+ *
+ * @brief An unofficial library for communicating with Waveshare E-Paper displays.
+ *
+ * @date 01-03-2020
+ * @author hypothermic <admin@hypothermic.nl>
+ */
