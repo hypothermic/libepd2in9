f6b37ec1092697cd569627b1277246c7d4fa3217
hypothermic <admin@hypothermic.nl>
2020-03-01 02:56:43 +0100

    Set the pin modes for the control pins
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- src/board/arduino/uno/spi.cpp
+++ src/board/arduino/uno/spi.cpp
@@ -8,20 +8,35 @@
 
 #include "private.h"
 
-#define DEFAULT_CS_PIN_TEMP (10) // temporary, see below
-
 NAMESPACE_BEGIN(LibEpd, Common)
 
+// All of the following definitions are temporary.
+// These should be configurable to the user's setup.
+// TODO create configurable CS pin
+#define __DEFAULT_CS_PIN_TEMP   (10)
+#define __DEFAULT_RST_PIN_TEMP  (8)
+#define __DEFAULT_DC_PIN_TEMP   (9)
+#define __DEFAULT_BUSY_PIN_TEMP (7)
+
 bool SPI::init() {
+    pinMode(__DEFAULT_CS_PIN_TEMP, OUTPUT);
+    pinMode(__DEFAULT_RST_PIN_TEMP, OUTPUT);
+    pinMode(__DEFAULT_DC_PIN_TEMP, OUTPUT);
+    pinMode(__DEFAULT_BUSY_PIN_TEMP, INPUT);
     SPIClass::begin();
     SPIClass::beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
     return true;
 }
 
 void SPI::write(BYTE value) {
-    IO::write(DEFAULT_CS_PIN_TEMP, LOW); // TODO create configurable CS pin
+    IO::write(__DEFAULT_CS_PIN_TEMP, LOW);
     SPIClass::transfer(value);
-    IO::write(DEFAULT_CS_PIN_TEMP, HIGH);
+    IO::write(__DEFAULT_CS_PIN_TEMP, HIGH);
 }
+
+#undef __DEFAULT_CS_PIN_TEMP
+#undef __DEFAULT_RST_PIN_TEMP
+#undef __DEFAULT_DC_PIN_TEMP
+#undef __DEFAULT_BUSY_PIN_TEMP
 
 NAMESPACE_END(LibEpd, Common)
